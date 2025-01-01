0cca82ee8cb0d450febbc2d5fdec04c7366be1f1
phuocbeo <quyphuoc2109@gmail.com>
2025-01-01 22:53:11 +0700

    CommentController

---
+++ src/main/java/com/example/demo/Controller/CommentController.java
@@ -0,0 +1,18 @@
+package com.example.demo.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.demo.Service.CommentService;
+
+import java.util.List;
+
+@Controller
+public class CommentController {
+
+        @Autowired
+
+}
---
+++ src/main/java/com/example/demo/Model/CommentModel.java
@@ -0,0 +1,71 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+@Entity
+public class CommentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String content;
+    @Column
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+    @ManyToOne
+    private UserModel user;
+    @ManyToOne
+    private PostModel post;
+    public CommentModel() {
+    }
+
+    public CommentModel(Long id, String content, Date date, UserModel user, PostModel post) {
+        this.id = id;
+        this.content = content;
+        this.date = date;
+        this.user = user;
+        this.post = post;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public UserModel getUser() {
+        return user;
+    }
+
+    public PostModel getPost() {
+        return post;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setUser(UserModel user) {
+        this.user = user;
+    }
+
+    public void setPost(PostModel post) {
+        this.post = post;
+    }
+}
